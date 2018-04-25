d3270dbd3ad1b4d13438d5f5420de816bd261c54
Unknown <bryn2.findlay-dykes@live.uwe.ac.uk>
2018-04-25 18:46:19 +0100

    level implementation complete

---
+++ Gameplay Programming Group 3/Assets/Scripts/scene1Specific/DoubleJumpSpwan.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJumpSpwan : MonoBehaviour {
+
+    public GameObject to_spawn;
+    public GameObject move_to_pos;
+
+    bool has_Spawned = false;
+
+    public GameObject door_unlock;
+    public GameObject door_move;
+
+    bool cam_once = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (has_Spawned == false)
+        {
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                to_spawn = Instantiate(to_spawn);
+                to_spawn.transform.position = transform.position;
+                has_Spawned = true;
+            }
+        }
+        else
+        {
+            to_spawn.transform.position = Vector3.MoveTowards(to_spawn.transform.position, move_to_pos.transform.position, Time.deltaTime * 5);
+            door_unlock.transform.position = Vector3.MoveTowards(door_unlock.transform.position, door_move.transform.position, Time.deltaTime * 5);
+
+            if (!cam_once)
+            {
+                Camera.main.transform.parent.transform.parent.GetComponent<CameraManager>().ScenicMode(2.5f, door_unlock.transform.position);
+                cam_once = true;
+            }
+        }
+    
+		
+	}
+}
