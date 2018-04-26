7f96f8248691e1b2b464c682a0fce5916eba8ad8
Andres Pereira <andres2.pereira@live.uwe.ac.uk>
2018-04-26 12:12:57 +0100

    Barrels launch player, started last scene transition

--- Gameplay Programming Group 3/Assets/Scripts/HurtPlayer.cs
+++ Gameplay Programming Group 3/Assets/Scripts/HurtPlayer.cs
@@ -16,6 +16,9 @@ public class HurtPlayer : MonoBehaviour
         if (col.collider.CompareTag("Player"))
         {
             player.GetComponent<PlayerMovement>().health--;
+            //player.GetComponent<PlayerMovement>().AddForce(air_launch * 200);
+            Vector3 force = player.transform.position - transform.position + Vector3.up;
+            player.GetComponent<PlayerMovement>().air_launch += new Vector3(force.x, force.y, 0);
             Destroy(gameObject);
         }
     }
